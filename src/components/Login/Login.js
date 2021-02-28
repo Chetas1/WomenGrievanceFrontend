94691912a7af4c6bb98c8c6a1dbe9d93b291579e
Chetas Patil <chetaspatil.7@gmail.com>
2021-02-28 14:53:01 +0530

    Added Login Screen

---
+++ src/components/Login/Login.js
@@ -0,0 +1,48 @@
+import { Modal, Button, Form } from "react-bootstrap"; 
+
+function LoginModal(props) {
+    return (
+      <Modal
+        {...props}
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="contained-modal-title-vcenter">
+            Login to Women Grievance
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+
+        <Form>
+            <Form.Group controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" placeholder="Enter email" />
+                <Form.Text className="text-muted">
+                    We'll never share your email with anyone else.
+                </Form.Text>
+            </Form.Group>
+
+            <Form.Group controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Password" />
+                <Form.Text className="text-muted">
+                    Your password will always be encrypted
+                </Form.Text>
+            </Form.Group>
+
+            <Button variant="primary" type="submit">
+                Log In
+            </Button>
+        </Form>
+
+        </Modal.Body>
+        <Modal.Footer>
+          <Button onClick={props.onHide}>Close</Button>
+        </Modal.Footer>
+      </Modal>
+    );
+  }
+  
+  export default LoginModal;
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,15 +1,22 @@
 import { Navbar,Nav } from "react-bootstrap"; 
+import Login from "../Login/Login";
+import React from 'react';
 
 function NavigationBar() {
+  const [modalShow, setModalShow] = React.useState(false);
+
     return (
+      <>
         <Navbar bg="dark" variant="dark">
-    <Navbar.Brand href="#home">Women Grievance</Navbar.Brand>
-    <Nav className="mr-auto">
-      <Nav.Link href="#home">Home</Nav.Link>
-      <Nav.Link href="#features">Mission</Nav.Link>
-      <Nav.Link href="#pricing">Login</Nav.Link>
-    </Nav>
-  </Navbar>
+          <Navbar.Brand href="#home">Women Grievance</Navbar.Brand>
+          <Nav className="mr-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Mission</Nav.Link>
+            <Nav.Link onClick={() => setModalShow(true)}>Login</Nav.Link>
+          </Nav>
+        </Navbar>
+        <Login show={modalShow} onHide={() => setModalShow(false)}/>
+      </>
     );
   }
   
