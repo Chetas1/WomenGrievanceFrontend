28523f6c6ecba0c3f655f9e7bf8a2dd06600c6b1
Chetas Patil <chetaspatil.7@gmail.com>
2021-03-06 18:05:24 +0530

    Login Component

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,6 +1,48 @@
+import React,{useState} from 'react';
 import { Modal, Button, Form } from "react-bootstrap"; 
+import LoginService from '../../service/LoginService';
 
 function LoginModal(props) {
+
+  let [userId, setUserId] = useState('');
+  let [password, setPassword] = useState('');
+
+  function setValueOfUserId(event) {
+    const { name, value } = event.target;
+    setUserId(value);
+  }
+
+  function setValueOfUserPassword(event) {
+    const { name, value } = event.target;
+    setPassword(value);
+  }
+
+  const fetchData = React.useCallback(() => {
+    LoginService.checkIfUserIsValid(userId,password).then(response => {
+          if(response.status === 200) { 
+            if(response.data)
+            {
+
+            }
+            else
+            {
+
+            }
+          }
+       })
+  }, [])
+
+  function submitForm(event) {
+    event.preventDefault();
+    console.log(userId);
+    fetchData();
+  }
+
+  React.useEffect(() => {
+    fetchData()
+  }, [fetchData])
+
+
     return (
       <Modal
         {...props}
@@ -15,10 +57,10 @@ function LoginModal(props) {
         </Modal.Header>
         <Modal.Body>
 
-        <Form>
+        <Form onSubmit={submitForm}>
             <Form.Group controlId="formBasicEmail">
                 <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" />
+                <Form.Control type="email" placeholder="Enter email" onChange={setValueOfUserId}/>
                 <Form.Text className="text-muted">
                     We'll never share your email with anyone else.
                 </Form.Text>
@@ -26,13 +68,13 @@ function LoginModal(props) {
 
             <Form.Group controlId="formBasicPassword">
                 <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" />
+                <Form.Control type="password" placeholder="Password" onChange={setValueOfUserPassword}/>
                 <Form.Text className="text-muted">
                     Your password will always be encrypted
                 </Form.Text>
             </Form.Group>
 
-            <Button variant="primary" type="submit">
+            <Button variant="primary" type="submit" >
                 Log In
             </Button>
         </Form>
